f16bc9c60c56e8379cf57b843624a81e2347303b
elidacon <elidacon@student.21-school.ru>
2024-04-03 21:48:05 +0500

    Swith 'x' fields on calc/graph mode change in the UI

--- src/s21_view.py
+++ src/s21_view.py
@@ -13,6 +13,11 @@ class View(QMainWindow, Ui_View):
         self._view_model = view_model
         self._exp_evaluated = False
 
+        self.calcMode.toggled.connect(self.on_calc_mode_toggled)
+        self.graphMode.toggled.connect(self.on_graph_mode_toggled)
+        self.calcMode.setChecked(True)
+        self.graphMode.setChecked(False)
+
         self._view_model.result_calculate_signal.connect(self.update_result)
         self._view_model.result_error_signal.connect(self.calculation_error)
         self.equal_press_signal.connect(self._view_model.calculate)
@@ -67,6 +72,14 @@ class View(QMainWindow, Ui_View):
     @Slot()
     def button_to_result_with_bracket(self):
         self.button_to_result(True)
+
+    # @Slot(bool)
+    def on_calc_mode_toggled(self, checked):
+        self.calcX.setVisible(checked)
+
+    # @Slot(bool)
+    def on_graph_mode_toggled(self, checked):
+        self.graphX.setVisible(checked)
 
     def on_equal_press(self):
         self.equal_press_signal.emit(self.expressionText.text())
