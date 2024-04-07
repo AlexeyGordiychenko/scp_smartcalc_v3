866292a7c85a01b3c6d2793e50015e97dbedebac
elidacon <elidacon@student.21-school.ru>
2024-04-07 20:39:23 +0500

    Add check_x_value() to VM to avoid duplicate code

--- src/s21_view_model.py
+++ src/s21_view_model.py
@@ -18,15 +18,18 @@ class ViewModel(QObject):
         except Exception as e:
             return np.nan
 
+    def check_x_value(self, x):
+        try:
+            return True, float(x)
+        except ValueError:
+            return False, 0
+
     def calculate_expression(self, expression, x):
-        if "x" in expression.lower():
-            try:
-                x = float(x)
-            except ValueError:
-                self.result_error_signal.emit("Invalid 'x' value")
-                return
-        else:
-            x = 0
+        x_exp = "x" in expression.lower()
+        check, x = self.check_x_value(x)
+        if x_exp and not check:
+            self.result_error_signal.emit("Invalid 'x' value")
+            return
         try:
             self._model.parse_expression(expression)
             self.result_calculate_signal.emit(self.calculate(x))
@@ -34,21 +37,22 @@ class ViewModel(QObject):
             self.result_error_signal.emit(str(e))
 
     def plot_graph(self, expression, x_min, x_max):
-        if "x" not in expression.lower():
-            raise ValueError("No 'x' in the expression, can't plot a graph")
-        else:
-            try:
-                x_min = float(x_min)
-                x_max = float(x_max)
-            except ValueError:
-                self.result_error_signal.emit("Invalid 'x' value")
-                return
+        x_exp = "x" in expression.lower()
+        check_min, x_min = self.check_x_value(x_min)
+        check_max, x_max = self.check_x_value(x_max)
+        if not x_exp:
+            self.result_error_signal.emit("No 'x' in the expression")
+            return
+        elif not check_min or not check_max:
+            self.result_error_signal.emit("Invalid 'x' value")
+            return
 
         try:
             self._model.parse_expression(expression)
         except Exception as e:
             self.result_error_signal.emit(str(e))
             return
+
         step = 0.01 if abs(x_max-x_min) <= 10 else 0.1
         x = np.arange(x_min, x_max, step)
         y = [self.calculate(x) for x in x]
