ae4a5ba425fda197684251c895443c83a58a12b7
elidacon <elidacon@student.21-school.ru>
2024-04-11 15:22:22 +0500

    Adapt s21_setup.py to both linux and windows

--- src/s21_setup.py
+++ src/s21_setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup, Extension
 import pybind11
+import os
+
+if os.name == 'nt':
+    cpp_args = ['/std:c++17']
+else:
+    cpp_args = ['-std=c++17']
 
 setup(
     name="s21_model_wrapper",
@@ -10,6 +16,7 @@ setup(
             ["s21_model_wrapper.cc", "s21_model.cc"],
             include_dirs=[pybind11.get_include()],
             language="c++",
+            extra_compile_args=cpp_args,
         ),
     ],
 )
