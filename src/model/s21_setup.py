b84e5b174e8d1a35d79775e22d3cced15fa60e38
elidacon <elidacon@student.21-school.ru>
2024-04-14 16:34:25 +0500

    Refactor: model/

--- src/model/s21_setup.py
+++ src/model/s21_setup.py
@@ -2,23 +2,27 @@ from setuptools import setup, Extension
 import pybind11
 import os
 
-if os.name == 'nt':
-    cpp_args = ['/std:c++17']
-else:
-    cpp_args = ['-std=c++17']
+if __name__ == "__main__":
+    # Different flags for linux and windows
+    if os.name == 'nt':
+        cpp_args = ['/std:c++17']
+    else:
+        cpp_args = ['-std=c++17']
 
-curr_dir = os.path.dirname(__file__)
-setup(
-    name="s21_model_wrapper",
-    version="0.1",
-    ext_modules=[
-        Extension(
-            "s21_model_wrapper",
-            [os.path.join(curr_dir, "s21_model_wrapper.cc"),
-             os.path.join(curr_dir, "s21_model.cc")],
-            include_dirs=[pybind11.get_include()],
-            language="c++",
-            extra_compile_args=cpp_args,
-        ),
-    ],
-)
+    # Get current path to connect C++ files
+    current_path = os.path.dirname(__file__)
+    # Setup the wrapper
+    setup(
+        name="s21_model_wrapper",
+        version="0.1",
+        ext_modules=[
+            Extension(
+                "s21_model_wrapper",
+                [os.path.join(current_path, "s21_model_wrapper.cc"),
+                 os.path.join(current_path, "s21_model.cc")],
+                include_dirs=[pybind11.get_include()],
+                language="c++",
+                extra_compile_args=cpp_args,
+            ),
+        ],
+    )
