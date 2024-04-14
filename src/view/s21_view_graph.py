a833934523242ef6af1897f43dcbb5d987521f1e
elidacon <elidacon@student.21-school.ru>
2024-04-14 19:06:36 +0500

    Refactor: ViewGraph

--- src/view/s21_view_graph.py
+++ src/view/s21_view_graph.py
@@ -7,12 +7,16 @@ class ViewGraph(QWidget):
     def __init__(self, title="Graph"):
         super().__init__()
 
+        # To free the window on close, because we store refs to graph windows in
+        # the View
         self.setAttribute(QtCore.Qt.WidgetAttribute.WA_DeleteOnClose)
-        self.setWindowTitle(title)
+        # Add the widget
         layout = QVBoxLayout()
         self._plot_widget = pg.PlotWidget()
         layout.addWidget(self._plot_widget)
         self.setLayout(layout)
+        # Set the window title
+        self.setWindowTitle(title)
 
     def plot_graph(self, x, y):
         self._plot_widget.plot(x, y)
